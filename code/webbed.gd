e42bbe4dd02b573ed6a82778ea591574d4786a28
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-06-07 12:30:28 +1200

    1:02 add web feature added
    if press t the web summon at ab and it will streach to be on both base and ab. press t to keep perm

--- code/webbed.gd
+++ code/webbed.gd
@@ -1,6 +1,8 @@
-extends Area2D
+extends CharacterBody2D
 var spider: Node2D
 var going = true
+var base_place = false
+var base_web_true = $".".global_position
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var spinder = get_tree().get_nodes_in_group('spider')
@@ -12,5 +14,18 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if going == true:
 		if get_tree().current_scene.scene_file_path == "res://scenes/main.tscn":
+			if base_place == false:
+				base_place = true
+				base_web_true = $".".global_position
 			print('a')
-			$".".look_at($"../../Spider/Spider/Node2D")
+			var spider_pos = spider.global_position
+			var web_base = $".".global_position
+			$".".look_at(spider_pos)
+			scale.x = sqrt(((spider_pos.x - web_base.x)*(spider_pos.x - web_base.x)) + ((spider_pos.y - web_base.y)*(spider_pos.y - web_base.y)))/2
+			$".".global_position.x = (base_web_true.x + spider_pos.x)/2
+			$".".global_position.y = (base_web_true.y + spider_pos.y)/2
+			
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_t"):
+		going = false
